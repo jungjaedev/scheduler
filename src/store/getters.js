ab3bedceab157b987d9e11c5f37f7541ad43c47c
jungjaedev <jungjaedev@gmail.com>
2023-01-04 14:04:39 +0900

    Add: store files added to branch off

---
+++ src/store/getters.js
@@ -0,0 +1,8 @@
+const storedMemoList = (state) => {
+  return state.memoList;
+};
+const storedCurrentData = (state) => {
+  return state.currentData;
+};
+
+export { storedCurrentData, storedMemoList };
